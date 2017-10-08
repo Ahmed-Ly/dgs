ef21cfd8c939128c4cc0fb16cffc9d8488e4a061
Ahmed Ly <lyahmed1998@gmail.com>
2017-10-08 07:00:02 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,7 +1,9 @@
-﻿function createTest()
-addEasingFunction("Pre_line",[[
+--[[
+function createTest()
+addEasingFunction("Pre_line",
 	return math.abs(value^2-0.5)*2
-]])
+)
+--]]
 
 wind = dgsDxCreateWindow(math.floor(0.2*sW),math.floor(0.3*sH),math.floor(0.4*sW),math.floor(0.4*sH),"Example Scroll Pane (exclude this window)",false)
 --pane = dgsDxCreateScrollPane(0,0,1,1,true,wind)
